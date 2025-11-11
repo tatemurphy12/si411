cd2a48843a47021d61c61c7946aef7ed2c526f1f
tatemurphy12 <jtatemurphy@gmail.com>
2025-11-11 10:33:18 -0500

    first commit

---
+++ lab03/sub.py
@@ -0,0 +1,36 @@
+#Tate Murphy
+#264542
+import re
+
+def main():
+    letters = open("letters.txt", "r")
+    mids = open("mids.txt", "w")
+    for line in letters:
+        l = []
+        subs = []
+        regex = r"^(?=.*m.*i.*d.*s)"
+        contains = False
+        for letter in line:
+            if letter == 'M':
+                l.append('m')
+                subs.append('m')
+            elif letter == 'I':
+                l.append('i')
+                subs.append('i')
+            elif letter == 'D':
+                l.append('d')
+                subs.append('d')
+            elif letter == 'S':
+                l.append('s')
+                subs.append('s')
+            else:
+                l.append(letter)
+            word = "".join(subs)
+        if re.search(regex, word):
+             contains = True
+        if contains == True:
+            mids.write("".join(l))
+            
+
+
+main()
